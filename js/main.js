ca5633a08ef9313e6b6fb10203d438c9acd0343d
Shroederrr <djbomzh@gmail.com>
2023-10-17 18:15:03 +0300

    Created index.html and main.js, which comput average of 3 numbers.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let firstNumb = +prompt("Enter first number:");
+let secondNumb = +prompt("Enter second number:");
+let thirdNumb = +prompt("Enter third number:");
+
+alert(`Average:
+${(firstNumb + secondNumb + thirdNumb)/3}
+`);
